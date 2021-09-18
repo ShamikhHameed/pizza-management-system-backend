f145889457e9072bd56860f3527ab7482efb1834
ShehaniFernando <it19011912@my.sliit.lk>
2021-09-18 20:45:38 +0530

    Topping Controller Modified

--- src/main/java/com/nsss/pizzamanagementsystembackend/controller/ToppingController.java
+++ src/main/java/com/nsss/pizzamanagementsystembackend/controller/ToppingController.java
@@ -1,9 +1,14 @@
 package com.nsss.pizzamanagementsystembackend.controller;
 
+import com.nsss.pizzamanagementsystembackend.model.Order;
+import com.nsss.pizzamanagementsystembackend.model.OrderItem;
 import com.nsss.pizzamanagementsystembackend.model.Topping;
 import com.nsss.pizzamanagementsystembackend.reponse.MessageResponse;
+import com.nsss.pizzamanagementsystembackend.reponse.ToppingStatistics;
+import com.nsss.pizzamanagementsystembackend.repository.OrderRepository;
 import com.nsss.pizzamanagementsystembackend.repository.ToppingRepository;
 import com.nsss.pizzamanagementsystembackend.request.ToppingRequest;
+import com.nsss.pizzamanagementsystembackend.request.ToppingStatisticsRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,15 +16,20 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
+//TOPPING CONTROLLER MODIFICATIONS
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/access")
 public class ToppingController {
     @Autowired
     ToppingRepository toppingRepository;
+
+    @Autowired
+    OrderRepository orderRepository;
 
     @GetMapping("/toppings")
     public ResponseEntity<List<Topping>> getAllToppings(@RequestParam(required = false) String toppingName) {
@@ -81,6 +91,55 @@ public class ToppingController {
             return new ResponseEntity<>(toppingRepository.save(_topping), HttpStatus.OK);
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/toppings/stats")
+    public ResponseEntity<List<ToppingStatistics>> getAllToppingStatistics(@Valid @RequestBody ToppingStatisticsRequest toppingStatisticsRequest) {
+        try {
+            Date fromTime = toppingStatisticsRequest.getFromTimestamp();
+            Date toTime = toppingStatisticsRequest.getToTimestamp();
+
+            List<ToppingStatistics> toppingStatistics = new ArrayList<>();
+
+            List<Topping> toppings = new ArrayList<>(toppingRepository.findAll());
+
+            if (toppings.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+            for(Topping topping : toppings){
+                toppingStatistics.add(new ToppingStatistics(topping.getId(), topping.getName(), 0, 0, 0, 0));
+            }
+
+            List<Order> orders = new ArrayList<>(orderRepository.findAllByOrderTimestampBetween(fromTime,
+                    toTime));
+
+            for(Order order : orders){
+                List<OrderItem> orderItems = order.getItems();
+
+                for(OrderItem orderItem : orderItems){
+                    for(ToppingStatistics toppingStatistic : toppingStatistics){
+                        if(toppingStatistic.getToppingName().equals(orderItem.getTopping())){
+                            if(orderItem.getSize().equals("small")){
+                                toppingStatistic.setSmallOrders(toppingStatistic.getSmallOrders() + orderItem.getQuantity());
+                            } else if(orderItem.getSize().equals("medium")){
+                                toppingStatistic.setMediumOrders(toppingStatistic.getMediumOrders() + orderItem.getQuantity());
+                            } else {
+                                toppingStatistic.setLargeOrders(toppingStatistic.getLargeOrders() + orderItem.getQuantity());
+                            }
+                        }
+                    }
+                }
+            }
+
+            for(ToppingStatistics toppingStatistic : toppingStatistics){
+                toppingStatistic.setTotalOrders(toppingStatistic.getSmallOrders() + toppingStatistic.getMediumOrders() + toppingStatistic.getLargeOrders());
+            }
+
+            return new ResponseEntity<>(toppingStatistics, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
